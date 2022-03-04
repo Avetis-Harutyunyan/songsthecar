2860ca67b6c5d11cb580224082ca96c2be906ac7
AvetisHarutyunyan <avetis.harutyunyan.98@gmail.com>
2022-03-04 13:45:15 +0400

    update songsthecar repository

---
+++ js/app.js
@@ -0,0 +1,26 @@
+function navSide() {
+    const bars = document.querySelector("#burger");
+    const nav = document.querySelector("#navSide");
+    const bodyLeft = document.querySelector("body");
+
+    bars.addEventListener("click", function() {
+        nav.classList.toggle("nav-active");
+        // burger animation
+        bars.classList.toggle("close");
+
+        // body to left
+        bodyLeft.classList.toggle("left");
+    });
+}
+navSide();
+
+
+function darkMode() {
+    const bulb = document.querySelector(".darkMode");
+    const body = document.querySelector("body");
+
+    bulb.addEventListener("click", function() {
+        body.classList.toggle("light");
+    });
+}
+darkMode();
--- scroll.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", function() {
-    const topNav = document.querySelector(".header__section");
-    topNav.classList.toggle("topNav", window.scrollY > 90);
-
-    const toTop = document.querySelector(".toTop");
-    toTop.classList.toggle("active", window.scrollY > 250);
-});
